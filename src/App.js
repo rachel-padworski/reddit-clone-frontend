87664437c15ddeaef3f0aac4aeff25776bd413ea
github username <github email address>
2020-12-06 09:23:56 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends React {
+  render() {
+    return (
+      <div className="App">
+        App 
+      </div>
+    );
+  }
+}
+
+export default App;
