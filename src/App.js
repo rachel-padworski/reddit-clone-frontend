7a6f9e2b03dad39c24172e13e691f4e439bcec4d
github username <github email address>
2020-12-07 10:40:40 -0700

    installed and imported packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends React {
+class App extends React.Component {
+  
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/posts/1/comments')
+  //     .then(response => response.json())
+  //     .then(posts => console.log(posts))
+  // }
+  
   render() {
     return (
       <div className="App">
